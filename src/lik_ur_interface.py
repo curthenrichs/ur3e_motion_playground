b3de3a34ad200d01c34f75703a86bf35a54d5234
HCI UW <wisconsinhci@gmail.com>
2020-03-11 00:42:50 -0500

    removed gripper update from joint update callback; must call separately

--- src/lik_ur_interface.py
+++ src/lik_ur_interface.py
@@ -17,8 +17,8 @@ target which may lead to large lag in respone to goal change.
 import yaml
 import rospy
 
-from std_msgs.msg import Empty, Bool
 from lively_ik.msg import JointAngles
+from std_msgs.msg import Empty, Bool, Float32
 from robotiq_85_msgs.msg import GripperCmd, GripperStat
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
@@ -29,7 +29,7 @@ DEFAULT_START_ENABLED = False
 
 class LikUrInterfaceNode:
 
-    def __init__(self, info_file_path, gripper_joint, joint_step_delay, start_enabled):
+    def __init__(self, info_file_path, joint_step_delay, start_enabled):
         self._joint_step_delay = joint_step_delay
         self._enabled = start_enabled
 
@@ -40,17 +40,12 @@ class LikUrInterfaceNode:
         self._arm_starting_config = y['starting_config']
         fin.close()
 
-        self._grip_idx = None
-        for i in range(0,len(self._arm_joint_names)):
-            name = self._arm_joint_names[i]
-            if name == gripper_joint:
-                self._grip_idx = i
-
         # ROS Publishers
         self._grip_cmd_pub = rospy.Publisher('gripper/cmd', GripperCmd, queue_size=10)
         self._traj_pub = rospy.Publisher('scaled_pos_traj_controller/command',JointTrajectory,queue_size=5)
 
         # ROS Subscribers
+        self._grip_position_sub = rospy.Subscriber('interface/gripper_position',Float32,self._grip_position_cb)
         self._enabled_sub = rospy.Subscriber('interface/enable',Bool,self._enabled_cb)
         self._ja_sub = rospy.Subscriber('relaxed_ik/joint_angle_solutions',JointAngles, self._ja_cb)
         self._set_initial_pose_sub = rospy.Subscriber('interface/set_initial_pose',Empty, self._set_initial_pose_cb)
@@ -62,16 +57,8 @@ class LikUrInterfaceNode:
 
     def _ja_cb(self, ja, enableOverride=False):
         if self._enabled or enableOverride:
-            print ja
-            if self._grip_idx != None:
-                pass
-                #grip = ja.angles.data[self._grip_idx]
-                #self._grip_cmd(grip)
-
             point = JointTrajectoryPoint()
             angles = list(ja.angles.data)
-            if self._grip_idx != None:
-                angles.pop(self._grip_idx)
             point.positions = angles
             point.time_from_start = rospy.Duration.from_sec(self._joint_step_delay)
             self._arm_move(point)
@@ -81,16 +68,13 @@ class LikUrInterfaceNode:
         traj.header.stamp = rospy.Time.now()
         traj.header.frame_id = 'world'
         names = list(self._arm_joint_names)
-        if self._grip_idx != None:
-            print len(names), self._grip_idx
-            names.pop(self._grip_idx)
         traj.joint_names = names
         traj.points = [p]
         self._traj_pub.publish(traj)
 
-    def _grip_cmd(self, ja):
+    def _grip_position_sub(self, msg):
         cmd = GripperCmd()
-        cmd.position = ja
+        cmd.position = msg.data # value between 0 (closed) and 0.085 (open)
         cmd.speed = 0.1
         cmd.force = 100
         self._grip_cmd_pub.publish(cmd)
@@ -99,15 +83,15 @@ class LikUrInterfaceNode:
         ja = JointAngles()
         ja.angles.data = self._arm_starting_config
         self._ja_cb(ja,True)
+        self._grip_position_cb(Float32(0.085))
 
 
 if __name__ == "__main__":
     rospy.init_node("lik_ur_interface")
 
     info_file_path = rospy.get_param('~info_file_path')
-    gripper_joint = rospy.get_param('~gripper_joint')
     joint_step_delay = rospy.get_param('~joint_step_delay',DEFAULT_JOINT_STEP_DELAY)
     start_enabled = rospy.get_param('~start_enabled',DEFAULT_START_ENABLED)
 
-    node = LikUrInterfaceNode(info_file_path, gripper_joint, joint_step_delay, start_enabled)
+    node = LikUrInterfaceNode(info_file_path, joint_step_delay, start_enabled)
     rospy.spin()
